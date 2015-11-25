2ead38d289d20c9f141d841a220cb0ebf0ed267d
brujoand <anders@brujordet.no>
2015-11-25 22:18:23 +0100

    Rudamentary re-election of troll

--- ekko.sh
+++ ekko.sh
@@ -1,11 +1,22 @@
-#! /bin/sh
+#! /bin/ash
 
 EKKO_IP=$(ip -o -4 addr list eth0 | grep -oE "([0-9]+[.])+[0-9]+")
 EKKO_MASK=$(ip -o -4 route | sed -En 's/([0-9]+[.][0-9]\/[0-9]+) .*/\1/p')
 EKKO_MULTICAST=224.1.0.1
+EKKO_TIMEOUT=30
 
 export EKKO_IP
 export EKKO_MASK
 export EKKO_MULTICAST
+export EKKO_TIMEOUT
 
-./tusse.sh || ./troll.sh
+log() {
+  echo "$EKKO_IP - $1"
+}
+
+while true; do
+  ./tusse.sh
+  if [[ $? -eq 1 ]]; then
+    ./troll.sh
+  fi
+done
--- tusse.sh
+++ tusse.sh
@@ -1,14 +1,23 @@
 #! /bin/sh
 
-sleep 1
+log() {
+  echo "$EKKO_IP - $1"
+}
 
-echo "Tusse: Entering the forest with multicast: ${EKKO_MULTICAST} mask: ${EKKO_MASK} ip: ${EKKO_IP}"
-echo "Tusse: Yelling for trolls"
+sleep 1 # TODO figure out why we need this
+
+log "Tusse: Entering the forest of: ${EKKO_MULTICAST} owned by the family: ${EKKO_MASK}"
+echo "Tusse: Calling for the master Troll"
 response=$(echo | socat STDIO UDP4-DATAGRAM:"${EKKO_MULTICAST}":6666,range="${EKKO_MASK}")
+heartbeats=hugs
 
 if [[ -n "$response" ]]; then
-  echo "Tusse: Jay, we found a troll: ${response}"
+  while [[ -n "$heartbeats" ]]; do
+   log "Tusse: Standing by for our master Troll:${response}"
+   heartbeats=$(timeout -t "${EKKO_TIMEOUT}" socat -d UDP4-RECVFROM:6666,ip-add-membership="${response}":"${EKKO_IP}",fork EXEC:"echo ${EKKO_IP}")
+  done
+  log "Tusse: Our troll is dead. We shall move on to the next master"
 else
-  echo "Tusse: I'm all alone. I'll be the troll"
+  log "Tusse: I'm all alone. I'll be the troll"
   exit 1
 fi
