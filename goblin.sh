f2c722768ed12c56d626d2c55dbf9d08be40081c
brujoand <anders@brujordet.no>
2015-11-26 02:43:46 +0100

    Filtering out killing the socat while listeing for troll shouts

--- goblin.sh
+++ goblin.sh
@@ -4,6 +4,8 @@ log() {
   echo "$EKKO_IP - Goblin: $1"
 }
 
+service_json="{\"service\":\"${SERVICE}\",\"ip\":\"${EKKO_IP}\", \"service_port\":\"${SERVICE_PORT}\"}"
+
 sleep 1 # TODO figure out why we need this
 
 log "Entering the forest of: ${EKKO_MULTICAST} owned by the family: ${EKKO_MASK}"
@@ -14,7 +16,8 @@ heartbeats=hugs
 if [[ -n "$response" ]]; then
   while [[ -n "$heartbeats" ]]; do
    log "Standing by for our master Troll:${response}"
-   heartbeats=$(timeout -t "${EKKO_TIMEOUT}" socat -d UDP4-RECVFROM:6666,ip-add-membership="${response}":"${EKKO_IP}",fork EXEC:"echo ${EKKO_IP}")
+   heartbeats=$(timeout -t "${EKKO_TIMEOUT}" socat -d UDP4-RECVFROM:6666,ip-add-membership="${response}":"${EKKO_IP}",fork EXEC:"echo $service_json"  2>&1 | grep 'Connection reset by peer')
+   echo "$heartbeats"
   done
   log "Our troll is dead. We shall move on to the next master"
 else
